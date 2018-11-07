a945654560af7424f00603cb807fb970e496190e
PSSTanusha <psstanusha@gmail.com>
2018-11-07 17:56:28 +0530

    Update subtraction.js

--- subtraction.js
+++ subtraction.js
@@ -4,3 +4,7 @@ function Sub(arr)
      console.log("The sum of "+arr[0]+" and "+arr[1]+"is: "+result);
 }
 Sub(process.argv.slice(2));
+
+Output:
+C:\Users\tanus\.vscode\extensions>node subtraction.js 98 78
+The sum of 98 and 78is: 20
