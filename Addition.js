b99cdade903a45edbe453e503bc3c76a4df924b1
PSSTanusha <psstanusha@gmail.com>
2018-11-07 22:23:02 +0530

    Update Addition.js

--- Addition.js
+++ Addition.js
@@ -1,9 +1,9 @@
 function addition(arr)
 {
-     console.log("The sum of "+arr[0]+" and "+arr[1]+"is: "+Number(arr[0])+Number(arr[1]));
+     console.log("The sum of "+arr[0]+" and "+arr[1]+" is: "+Number(arr[0])+Number(arr[1]));
 }
 addition(process.argv.slice(2));
 
 output:
 C:\Users\tanus\.vscode\extensions>node addition.js 6789 9087
-The sum of 6789 and 9087is: 67899087
+The sum of 6789 and 9087 is: 67899087
