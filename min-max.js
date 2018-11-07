f52e50b8cba2d545919411eee73d47d568bc8ee4
PSSTanusha <psstanusha@gmail.com>
2018-11-07 22:36:05 +0530

    Update min-max.js

--- min-max.js
+++ min-max.js
@@ -11,3 +11,7 @@ function Min_Max(arr)
     console.log("The minimum element is "+min+" and the maximum element is "+max);
 }
 Min_Max(process.argv.slice(2));
+
+output:
+C:\Users\tanus\.vscode\extensions>node min-max.js 55 46 78 90 22 11
+The minimum element is 11 and the maximum element is 90
