746d7b44c64a822a552c4bc2df4e2bf9ad79fce9
PSSTanusha <psstanusha@gmail.com>
2018-11-08 11:01:43 +0530

    Update multiplication.js

--- multiplication.js
+++ multiplication.js
@@ -5,3 +5,7 @@ function Multiplication(arr)
     console.log("The product of "+num1+" and "+num2+" is: "+num1*num2);
 }
 Multiplication(process.argv.slice(2));
+
+output:
+C:\Users\tanus\.vscode\extensions>node multiplication.js 7 19
+The product of 7 and 19 is: 133
