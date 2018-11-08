bcdbe452eaef46e5edfc27c722466da9e7e9f677
PSSTanusha <psstanusha@gmail.com>
2018-11-08 13:23:38 +0530

    Update divison.js

--- divison.js
+++ divison.js
@@ -5,3 +5,7 @@ function Divison(arr)
    console.log("The divison of "+num1+" and "+num2+" is:"+num1/num2);
 }
 Divison(process.argv.slice(2));
+
+output:
+C:\Users\tanus\.vscode\extensions>node divison.js 81 9
+The divison of 81 and 9 is:9
